be731772486f1cc99bbc124202406296cd23fbae
Z-928 <56246485+Z-928@users.noreply.github.com>
2021-11-15 23:20:33 +0900

    Update buggy.py

--- Terra-0-4000/8/buggy.py
+++ Terra-0-4000/8/buggy.py
@@ -3,4 +3,4 @@ from qiskit import *
 q = QuantumRegister(1) 
 qc = QuantumCircuit(q) 
 job = execute(qc, backend='local_statevector_simulator') 
-job.result().get_data(qc).
+job.result().get_data(qc)
