13e6be3cc472f9be24328e9d9adc6835ed4254f6
Z-928 <56246485+Z-928@users.noreply.github.com>
2021-11-15 23:00:23 +0900

    Update Fixed.py

--- Terra-0-4000/3/Fixed.py
+++ Terra-0-4000/3/Fixed.py
@@ -1,5 +1,5 @@
 from qiskit import QuantumCircuit
 
 qc = QuantumCircuit(3)
-qc.cx(0, 1, label='Label', ctrl_state=0)
-qc.ccx(0, 1, 2, ctrl_state=1)
+qc.cx(0, 1, ctrl_state='0')
+qc.ccx(0, 1, 2, ctrl_state='00')
